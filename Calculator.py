58440c339a7d41a019c126a0010003666f1b8a72
rohitbiswas1 <108587295+rohitbiswas1@users.noreply.github.com>
2022-12-06 21:26:03 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+#This is for Calculation
+a = 74
+b = 36
+x = a+b
+print("Addition of",a,"and",b,"is",x)
+y = a-b
+print("sub of",a,"and",b,"is",y)
+z = a*b
+print("Multi of",a,"and",b,"is",z)
+d = a/b
+print("Div of",a,"and",b,"is",d)
+M = a%b
+print("Module of",a,"and",b,"is",M)
