e4ee21c4c0934e4e3065c3392d7f23cfb5a53c34
rohitbiswas1 <108587295+rohitbiswas1@users.noreply.github.com>
2025-04-30 19:41:39 +0530

    Variable.py

---
+++ Variable.py
@@ -0,0 +1,60 @@
+                        #Variables in Python
+
+#Variables are used to store data in a program.
+name="Rohit"
+age= 18
+height = 5.8
+weight = 54.7
+
+print("My nameb is :",name )
+print("My age is:",age )
+print("My height and weight is: " , height ,"and", weight)  
+
+
+ #type() function is used to check the data type of a variable 
+print(type(name))
+print(type(age))
+
+                          #DATA TYPES IN PYTHON
+
+
+ #string data type 
+name1='Ram'
+name2="Ram"
+name3='''Ram'''
+
+print(name1)
+print(name2)
+print(name3)
+
+ #integer data type
+age1=19
+age2=78
+height1=67
+height2=64
+
+print("Height and age of first person is:", height1,"and" ,age1)
+print("Height and age of second person is:", height2,"and", age2)
+
+ #float data type
+nameA="Rohit"
+nameB="Rahul"
+salary=10000.67
+salary1=63882.75
+
+print(nameA,"salary " ,  "is",salary)
+print(nameB,"salary ", "is",salary1)
+
+#boolean data type
+ageA=54
+old=True
+ageB=35
+old1=False
+print(ageA,"This person is old!",old)
+print(ageB,"This person is old!",old1)
+ 
+ #None data type
+nameC=None
+nameD=None
+print(type(nameC))
+print(type(nameD))
