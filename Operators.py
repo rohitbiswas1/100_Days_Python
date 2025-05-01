c0987ec59f2cbd12eddbb8e21ce183b294031c1e
rohitbiswas1 <108587295+rohitbiswas1@users.noreply.github.com>
2025-05-01 18:01:42 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,151 @@
+    # Arithmetic Operators 
+    # Arithmetic operators are used to perform arithmetic operations on variables and values.
+
+    #Addition of the variables
+
+a=8
+b=2
+sum=a+b
+print("Sum of",a,"and",b,"is",sum)
+    #Difference of the variables
+d=5
+e=8
+diff=d-e
+print("Difference of",d,"and",e,"is",diff)
+    #Product of the variables
+p=4  
+q=5 
+product=p*q     
+print("Product of",p,"and",q,"is",product)
+    #Division of the variables
+d=8
+e=2
+div=d/e
+print("Division of",d,"and",e,"is",div)
+    #Modulus of the variables
+m=9
+n=2
+mod=m%n
+print("Modulus of",m,"and",n,"is",mod)
+  #Power of the variable 
+z= 9
+v = 2
+power = z**v
+print("Power of",z,"and",v,"is",power)
+
+
+
+ #Relational Operators
+ #Relational operators are used to compare two values.
+ 
+ #Equal to operator
+a = 6
+b = 3
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not greater than b")
+
+ #Not equal to operator
+w = 67
+t = 45
+if w != t:
+    print("w is not equal to t")
+else:
+    print("w is equal to t")
+ #Greater than operator
+f = 6
+g = 3
+if f > g:
+    print("f is greater than g") 
+else:   
+    print("f is not greater than g")
+#Greater than or equal to operator
+L = 6
+N = 6
+if L >= N:
+    print("L is greater than or equal to N")
+else:
+    print("L is not greater than or equal to N")
+#Less than operator
+h = 3
+i = 6
+if h < i:
+    print("h is less than i")
+else:   
+    print("h is not less than i")
+#Less than or equal to operator
+j = 3
+k = 3
+if j <= k:
+    print("j is less than or equal to k")
+else:
+    print("j is not less than or equal to k")
+
+
+
+    #Assignment Operators
+    #Assignment operators are used to assign values to variables.
+ #Equal to operator     
+num = 9 
+s = 3
+num = s
+print("Value of num is", num)
+    #Add and assign operator
+num1 = 5
+num2 = 3
+num1 += num2
+print("Value of num1 is", num1)
+    #Subtract and assign operator
+num3 = 5
+num4 = 3
+num3 -= num4
+print("Value of num3 is", num3)
+    #Multiply and assign operator
+num5 = 5
+num6 = 3
+num5 *= num6
+print("Value of num5 is", num5)
+    #Divide and assign operator
+num7 = 5
+num8 = 3
+num7 /= num8
+print("Value of num7 is", num7)
+    #Modulus and assign operator
+num9 = 5
+num10 = 3
+num9 %= num10
+print("Value of num9 is", num9)
+    #Power and assign operator
+num11 = 5
+num12 = 3
+num11 **= num12
+print("Value of num11 is", num11)
+    
+  #Logical Operator 
+#Logical operators are used to combine conditional statements.
+
+#Not operator 
+is_raining = True
+
+if not is_raining:
+    print("You can go outside without an umbrella.")
+else:
+    print("You need an umbrella.")
+#And operator
+is_sunny = True
+is_warm = True
+print("Is it sunny and warm?")
+if is_sunny and is_warm:
+    print("Yes, it's a nice day!")
+else:
+    print("No, it's not a nice day.")
+#Or operator
+is_cold = False
+is_windy = True
+print("Is it cold or windy?")
+if is_cold or is_windy:
+    print("Yes, it's not a good day to go outside.")
+else:
+    print("No, it's a good day to go outside.")
+
