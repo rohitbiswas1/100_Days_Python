3c9579bf55eff8046740ccb5740a79f2d7de328e
rohitbiswas1 <108587295+rohitbiswas1@users.noreply.github.com>
2025-05-04 07:13:49 +0530

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,22 @@
+# Taking input from the user
+user_input = input("Enter your name: ")
+
+# Printing the input
+print("Hello-->", user_input)
+print("Welcome to the program!")
+
+#Type of input
+print("Type of input:", type(user_input), "Value of input:", user_input)
+
+
+
+
+#Simple input example
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+marks = float(input("Enter your marks: "))
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
