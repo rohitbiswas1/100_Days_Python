0b4a8e73a44e00b21e99d53ac4b7d36e0627d5b5
Rohit Biswas <108587295+rohitbiswas1@users.noreply.github.com>
2025-12-29 22:25:40 +0530

    Update value_by_index.py

--- value_by_index.py
+++ value_by_index.py
@@ -1,8 +1,9 @@
 # Write a program in Python to search a value by index.
 
-numbers = [10, 20, 30, 40, 50]
+numbers = [10, 20, 30, 40, 58]
 index = int(input("Enter index to search value: "))
 if 0 <= index < len(numbers):
     print("The value at index", index, "is:", numbers[index])
 else:
     print("Invalid index!")
+
